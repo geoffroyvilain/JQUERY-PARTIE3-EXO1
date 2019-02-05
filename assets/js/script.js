9ab610ec99bf2602c86c30063f901bdf12633735
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-02-05 15:01:21 +0100

    exo corrigé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
+//Construisez une page html avec un bouton et un champ texte dans lequel on affiche le nombre de clics sur le bouton.
 $(function() {
-  var count = 0;
+//nombre de coups
+  var count = 0; // à mettre en dehors de la fonction sinon réinitialisé à 0 à chaque clic
   $('#clickMe').click(function() {
-    count++;
-    $('#text').text(count);
+    count++; //AJOUTE 1 AU COMPTEUR
+// '#text' permet d'écrire du texte / val permet de definir la valeur
+    $('#text').val(count);
   });
 });
